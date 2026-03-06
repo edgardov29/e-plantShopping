79b47851f8d4bc3cc4b6a16bb6cac656cc42943f
edgardov29 <edgardovelasco29@gmail.com>
2026-03-06 16:36:22 -0500

    cartItem casi terminados

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -30,19 +30,32 @@ const CartItem = ({ onContinueShopping }) => {
 
 
   const handleIncrement = (item) => {
-    dispatch(updateQuantity({name: item.name,
-                             quantity: item.quantity + 1}))
+    dispatch(updateQuantity({
+        name: item.name,
+        quantity: item.quantity + 1
+    }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1){
+        dispatch(updateQuantity({
+            name: item.name,
+            quantity: item.quantity - 1
+        }));
+    }else{
+        dispatch(removeItem({name: item.name}))
+    }
+    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({name: item.name}))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let item_total = item.quantity * parseFloat(item.cost.substring(1));
+    return item_total
   };
 
   return (
