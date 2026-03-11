d80c788075ea66b9d589eaa76350eb7429c1b076
edgardov29 <edgardovelasco29@gmail.com>
2026-03-11 17:03:10 -0400

    Agregando codigo a funciones de CartItem

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -24,8 +24,12 @@ export const CartSlice = createSlice({
         state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
+        const {name, quantity} = action.payload;
+        const itemToUpdate = state.items.find(item => item.name === name);
+        if (itemToUpdate){
+            itemToUpdate.quantity = quantity;// asigna directamente el nuevo valor
 
-    
+        }
     },
   },
 });
